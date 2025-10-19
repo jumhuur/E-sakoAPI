49a6fd213aa9ba824b88c95a9c659f1d0d45b8c3
Jumhuur <Jumhuur123@hotmail.com>
2025-10-19 17:58:48 +0300

    db in not now

--- database.py
+++ database.py
@@ -1,15 +1,15 @@
 
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-from dotenv import load_dotenv
-import os
+# from pymongo.mongo_client import MongoClient
+# from pymongo.server_api import ServerApi
+# from dotenv import load_dotenv
+# import os
 
-load_dotenv()
+# load_dotenv()
 
-uri = os.getenv("URI")
+# uri = os.getenv("URI")
 
-client = MongoClient(uri, server_api=ServerApi("1"))
+# client = MongoClient(uri, server_api=ServerApi("1"))
 
 
-db = client.esakoapi
-collection = db["Sakawaat"]
+# db = client.esakoapi
+# collection = db["Sakawaat"]
--- sako_models.py
+++ sako_models.py
@@ -1,6 +1,6 @@
-from pydantic import BaseModel
-class skawaat(BaseModel):
-    item:str
-    call_counts:int
-    last_call:str
+# from pydantic import BaseModel
+# class skawaat(BaseModel):
+#     item:str
+#     call_counts:int
+#     last_call:str
 
